3a7838cf2b04a084a119878db81392cdbd223d83
Batjaa Batbold <batjaa0615@gmail.com>
2019-05-17 13:21:07 +0900

    Allow ajax form submission

--- themes/mongkok/static/js/contact.js
+++ themes/mongkok/static/js/contact.js
@@ -1,26 +1,23 @@
-// jQuery(document).ready(function(){
+jQuery(document).ready(function(){
 
-// 	$('#contact-form').submit(function(){
-// 		var action = $(this).attr('action'),
-// 			$spinner = $('<i />').addClass('fa fa-lg fa-spin fa-spinner');
-// 			originalSubmitBtnHtml = $('#contact-form-submit-btn').attr('disabled','disabled').html();
+	$('#contact-form').submit(function(){
+		var action = $(this).attr('action'),
+			$spinner = $('<i />').addClass('fa fa-lg fa-spin fa-spinner');
+			originalSubmitBtnHtml = $('#contact-form-submit-btn').attr('disabled','disabled').html();
 
-// 		$('#contact-form-submit-btn').html('').append($spinner);
-// 		$('#message').empty();
+		$('#contact-form-submit-btn').html('').append($spinner);
+		$('#message').empty();
 
-// 		$.post(action, {
-// 				name: $('#contact-form-name').val(),
-// 				email: $('#contact-form-email').val(),
-// 				comments: $('#contact-form-message').val(),
-// 			},
-// 			function(data){
-// 				$('#message').html(data);
-// 				$('#contact-form-submit-btn').html(originalSubmitBtnHtml).removeAttr('disabled');
-// 				if(data.match('Thank you') !== null) $('#contact-form').slideUp('fast');
-// 			}
-// 		);
+	  var $form = $(this);
+		$.post(action, $form.serialize(),
+			function(data){
+				$('#message').html(data);
+				$('#contact-form-submit-btn').html(originalSubmitBtnHtml).removeAttr('disabled');
+				// if(data.match('Thank you') !== null) $('#contact-form').slideUp('fast');
+			}
+		);
 
-// 		return false;
-// 	});
+		return false;
+	});
 
-// });
+});
