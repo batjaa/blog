7d871b07e1104aa7123762fcd0da80d2f4a7f585
Batjaa Batbold <batjaa0615@gmail.com>
2019-05-27 21:21:43 +0800

    Attempt fixing js form submission

--- themes/mongkok/static/js/contact.js
+++ themes/mongkok/static/js/contact.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function(){
 
-	$('#contact-form').submit(function(){
+	$('#contact-form').submit(function(e) {
+		e.preventDefault();
+
 		var action = $(this).attr('action'),
 			$spinner = $('<i />').addClass('fa fa-lg fa-spin fa-spinner');
 			originalSubmitBtnHtml = $('#contact-form-submit-btn').attr('disabled','disabled').html();
@@ -9,13 +11,12 @@ jQuery(document).ready(function(){
 		$('#message').empty();
 
 	  var $form = $(this);
-		$.post(action, $form.serialize(),
-			function(data){
+		$.post(action, $form.serialize())
+			.then(function (data) {
 				$('#message').html(data);
 				$('#contact-form-submit-btn').html(originalSubmitBtnHtml).removeAttr('disabled');
 				// if(data.match('Thank you') !== null) $('#contact-form').slideUp('fast');
-			}
-		);
+			});
 
 		return false;
 	});
