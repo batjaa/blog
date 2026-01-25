c91471b90142e5ec68d12cdfb1c2c45db2f2d240
Batjaa Batbold <batjaa0615@gmail.com>
2026-01-24 21:44:20 -0800

    feat: update react-email

--- newsletter/scripts/build.ts
+++ newsletter/scripts/build.ts
@@ -1,4 +1,4 @@
-import { render } from "@react-email/components";
+import { render } from "@react-email/render";
 import * as fs from "fs";
 import * as path from "path";
 import { config } from "dotenv";
